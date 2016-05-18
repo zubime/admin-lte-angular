90f59e5c77bafa1fdcde8484fb6a193a6bdd338e
Filip Matuszewski <filip.matuszewski@gmail.com>
2016-05-18 18:52:53 +0200

    changes

--- app/lte-box-widget-directive.js
+++ app/lte-box-widget-directive.js
@@ -8,7 +8,15 @@
    * @element
    *
    * @description
+   * BoxWidget
+   * =========
+   * lteBoxWidget is a plugin to handle collapsing and
+   * removing boxes from the screen.
    *
+   * @type Object
+   * @usage $.AdminLTE.boxWidget.activate()
+   *        Set all your options in the main $.AdminLTE.options object
+   *   *
    * @example
      <example module="adminLteAngular">
        <file name="index.html">
@@ -24,84 +32,21 @@
   function lteBoxWidget() {
     return {
       restrict: 'EA',
-      scope: {},
+      scope: {
+        options: '='
+      },
       templateUrl: '/lte-box-widget-directive.tpl.html',
       replace: false,
       controllerAs: 'lteBoxWidget',
       controller: function () {
-        var vm = this;
-        vm.name = 'lteBoxWidget';
+        var _this = this;
+        _this.name = 'lteBoxWidget';
       },
       link: function (scope, element, attrs) {
         /* jshint unused:false */
         /* eslint "no-unused-vars": [2, {"args": "none"}] */
       }
+
     };
   }
 }());
-
-
-
-/* BoxWidget
- * =========
- * BoxWidget is a plugin to handle collapsing and
- * removing boxes from the screen.
- *
- * @type Object
- * @usage $.AdminLTE.boxWidget.activate()
- *        Set all your options in the main $.AdminLTE.options object
- */
-$.AdminLTE.boxWidget = {
-  selectors: $.AdminLTE.options.boxWidgetOptions.boxWidgetSelectors,
-  icons: $.AdminLTE.options.boxWidgetOptions.boxWidgetIcons,
-  animationSpeed: $.AdminLTE.options.animationSpeed,
-  activate: function (_box) {
-    var _this = this;
-    if (!_box) {
-      _box = document; // activate all boxes per default
-    }
-    //Listen for collapse event triggers
-    $(_box).on('click', _this.selectors.collapse, function (e) {
-      e.preventDefault();
-      _this.collapse($(this));
-    });
-
-    //Listen for remove event triggers
-    $(_box).on('click', _this.selectors.remove, function (e) {
-      e.preventDefault();
-      _this.remove($(this));
-    });
-  },
-  collapse: function (element) {
-    var _this = this;
-    //Find the box parent
-    var box = element.parents(".box").first();
-    //Find the body and the footer
-    var box_content = box.find("> .box-body, > .box-footer, > form  >.box-body, > form > .box-footer");
-    if (!box.hasClass("collapsed-box")) {
-      //Convert minus into plus
-      element.children(":first")
-        .removeClass(_this.icons.collapse)
-        .addClass(_this.icons.open);
-      //Hide the content
-      box_content.slideUp(_this.animationSpeed, function () {
-        box.addClass("collapsed-box");
-      });
-    } else {
-      //Convert plus into minus
-      element.children(":first")
-        .removeClass(_this.icons.open)
-        .addClass(_this.icons.collapse);
-      //Show the content
-      box_content.slideDown(_this.animationSpeed, function () {
-        box.removeClass("collapsed-box");
-      });
-    }
-  },
-  remove: function (element) {
-    //Find the box parent
-    var box = element.parents(".box").first();
-    box.slideUp(this.animationSpeed);
-  }
-};
-}
--- app/lte-treeview-menu-directive.js
+++ app/lte-treeview-menu-directive.js
@@ -8,6 +8,13 @@
    * @element
    *
    * @description
+   * Tree()
+   * ======
+   * Converts the sidebar into a multilevel
+   * tree view menu.
+   *
+   * @type Function
+   * @Usage: $.AdminLTE.tree('.sidebar')
    *
    * @example
      <example module="adminLteAngular">
@@ -17,34 +24,7 @@
      </example>
    *
    */
-  angular
-    .module('adminLteAngular')
-    .directive('treeviewMenu', lteTreeviewMenu);
-
-  function lteTreeviewMenu() {
-    return {
-      restrict: 'C',
-      scope: {},
-      templateUrl: '/lte-treeview-menu-directive.tpl.html',
-      replace: true,
-      transclude: true,
-      controllerAs: 'lteTreeviewMenu',
-      controller: function () {
-        var vm = this;
-        vm.name = 'lteTreeviewMenu';
-      },
-      link: function (scope, element, attrs) {
-        /* jshint unused:false */
-        /* eslint "no-unused-vars": [2, {"args": "none"}] */
-        element.on('click','li.treeview a',function(e){
-          
-        });
-      }
-    };
-  }
-}());
-
-
+//jQuery
 /* Tree()
  * ======
  * Converts the sidebar into a multilevel
@@ -53,48 +33,81 @@
  * @type Function
  * @Usage: $.AdminLTE.tree('.sidebar')
  */
-$.AdminLTE.tree = function (menu) {
-  var _this = this;
-  var animationSpeed = $.AdminLTE.options.animationSpeed;
-  $(menu).on('click', 'li a', function (e) {
-    //Get the clicked link and the next element
-    var $this = $(this);
-    var checkElement = $this.next();
 
-    //Check if the next element is a menu and is visible
-    if ((checkElement.is('.treeview-menu')) && (checkElement.is(':visible')) && (!$('body').hasClass('sidebar-collapse'))) {
-      //Close the menu
-      checkElement.slideUp(animationSpeed, function () {
-        checkElement.removeClass('menu-open');
-        //Fix the layout in case the sidebar stretches over the height of the window
-        //_this.layout.fix();
-      });
-      checkElement.parent("li").removeClass("active");
-    }
-    //If the menu is not visible
-    else if ((checkElement.is('.treeview-menu')) && (!checkElement.is(':visible'))) {
-      //Get the parent menu
-      var parent = $this.parents('ul').first();
-      //Close all open menus within the parent
-      var ul = parent.find('ul:visible').slideUp(animationSpeed);
-      //Remove the menu-open class from the parent
-      ul.removeClass('menu-open');
-      //Get the parent li
-      var parent_li = $this.parent("li");
 
-      //Open the target menu and add the menu-open class
-      checkElement.slideDown(animationSpeed, function () {
-        //Add the class active to the parent li
-        checkElement.addClass('menu-open');
-        parent.find('li.active').removeClass('active');
-        parent_li.addClass('active');
-        //Fix the layout in case the sidebar stretches over the height of the window
-        _this.layout.fix();
-      });
-    }
-    //if this isn't a link, prevent the page from being redirected
-    if (checkElement.is('.treeview-menu')) {
-      e.preventDefault();
-    }
-  });
-};
+   //Angular
+  angular
+    .module('adminLteAngular')
+    .directive('lteTreeviewMenu', lteTreeviewMenu);
+
+  function lteTreeviewMenu() {
+
+    function link(scope, element, attrs) {
+
+     if(!scope.options){scope.options={};}
+     if(!scope.options.animationSpeed){scope.options.animationSpeed = 200;}
+     var animationSpeed = scope.options.animationSpeed;
+
+        element.on('click','a', function (e) {
+
+       //Get the clicked link and the next element
+       var $this = angular.element(this);
+       var checkElement = $this.next();
+
+       //Check if the next element is a menu and is visible
+       if ((checkElement.is('.treeview-menu')) && (checkElement.is(':visible')) && (!angular.element('body').hasClass('sidebar-collapse'))) {
+         //Close the menu
+         checkElement.slideUp(animationSpeed, function () {
+           checkElement.removeClass('menu-open');
+           //Fix the layout in case the sidebar stretches over the height of the window
+           //_this.layout.fix();
+         });
+         checkElement.parent("li").removeClass("active");
+       }
+       //If the menu is not visible
+       else if ((checkElement.is('.treeview-menu')) && (!checkElement.is(':visible'))) {
+         //Get the parent menu
+         var parent = $this.parents('ul').first();
+         //Close all open menus within the parent
+         var ul = parent.find('ul:visible').slideUp(animationSpeed);
+         //Remove the menu-open class from the parent
+         ul.removeClass('menu-open');
+         //Get the parent li
+         var parent_li = $this.parent("li");
+
+         //Open the target menu and add the menu-open class
+         checkElement.slideDown(animationSpeed, function () {
+           //Add the class active to the parent li
+           checkElement.addClass('menu-open');
+           parent.find('li.active').removeClass('active');
+           parent_li.addClass('active');
+           //Fix the layout in case the sidebar stretches over the height of the window
+           //element.parent('body').layout.fix();
+         });
+       }
+       //if this isn't a link, prevent the page from being redirected
+       if (checkElement.is('.treeview-menu')) {
+         e.preventDefault();
+       }
+     });
+   };
+
+
+    return {
+      restrict: 'EC',
+      transclude: true,
+      scope: {
+        options: '=options',
+        name: '@name',
+        items: '=items'
+      },
+      templateUrl: '/lte-treeview-menu-directive.tpl.html',
+      replace: true,
+      controller: function () {
+        var vm = this;
+        vm.name = 'lteTreeviewMenu';
+      },
+      link: link
+    };
+  }
+}());

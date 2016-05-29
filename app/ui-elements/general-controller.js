02f8a15d46230cebc9089b4f218bbcbb2798c1e9
Filip Matuszewski <filip.matuszewski@gmail.com>
2016-05-29 09:20:32 +0200

    Accordion test

--- app/ui-elements/general-controller.js
+++ app/ui-elements/general-controller.js
@@ -15,5 +15,24 @@
   function GeneralCtrl() {
     var vm = this;
     vm.ctrlName = 'GeneralCtrl';
+
+    vm.groups = [
+      {
+        title: 'Dynamic Group Header - 1',
+        content: 'Dynamic Group Body - 1'
+      },
+      {
+        title: 'Dynamic Group Header - 2',
+        content: 'Dynamic Group Body - 2'
+      }
+    ];
+
+    vm.items = ['Item 1', 'Item 2', 'Item 3'];
+
+    vm.addItem = function() {
+      var newItemNo = $scope.items.length + 1;
+      $scope.items.push('Item ' + newItemNo);
+    };
+
   }
 }());
